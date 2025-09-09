8b3515683b70689ae6d023fb09dce96cba0dbde9
Hayotunday <idowudanielayotunde@gmail.com>
2025-09-09 21:48:09 +0100

    Update layout.tsx metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Adebayo Paul - Moniepoint',
   description: 'Created with v0',
-  generator: 'v0.app',
 }
 
 export default function RootLayout({
